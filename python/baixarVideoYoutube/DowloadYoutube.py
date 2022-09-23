64535db2f91e2206515262af92138caf6cb17270
elievelton <elievelton@gmail.com>
2022-09-22 23:05:16 -0300

    Atualizações

--- python/baixarVideoYoutube/DowloadYoutube.py
+++ python/baixarVideoYoutube/DowloadYoutube.py
@@ -1,15 +1,18 @@
 import PySimpleGUI as sg
 from pytube import YouTube
 
+#Função apra pegar o link de download e a pasta para guardar o video
 def executarDownload(link, path):
     video = YouTube(link)
     video.streams.get_highest_resolution().download(output_path=path)
 
-
+# Define o conteúdo da tela
 layout = [[sg.Text("link para Download do vídeo"), sg.InputText('')],
           [sg.Text("Escolha a pasta para salvar"), sg.InputText(''),sg.FolderBrowse()],
-          [sg.Button('Baixar'), sg.Button('Cancelar')]
+          [sg.Button('Baixar'), sg.Button('Cancelar'), sg.Text('Versão de Demo 1.0 para Desktop.')],
+          
           ]
+# Nomeando e criando a tela
 janela = sg.Window("DownLoadYouTube", layout)
 
 while True:
